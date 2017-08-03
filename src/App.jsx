e1251e65baa49bbadb9e5c23b3d2dd9b4df73448
Sam Coll <samcoll@gmail.com>
2017-08-03 15:00:18 -0700

    Updating App.jsx to construct its own state, then pass through data in that state to its rendered components as props. The components then use the props to populate content within the HTML being rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,46 @@ import MessageList from './MessageList.jsx';
 import NavBar from './NavBar.jsx';
 
 class App extends Component {
+    componentDidMount() {
+        console.log("componentDidMount <App />");
+        setTimeout(() => {
+            console.log("Simulating incoming message");
+            // Add a new message to the list of messages in the data store
+            const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+            const messages = this.state.messages.concat(newMessage)
+            // Update the state of the app component.
+            // Calling setState will trigger a call to render() in App and all child components.
+            this.setState({messages: messages})
+        }, 3000);
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+            messages: [
+                {
+                    id: 1,
+                    username: "Bob",
+                    content: "Has anyone seen my marbles?",
+                },
+                {
+                    id: 2,
+                    username: "Anonymous",
+                    content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 <NavBar />
-                <MessageList />
-                <ChatBar />
+                <MessageList messages={ this.state.messages } />
+                <ChatBar currentUser={ this.state.currentUser } />
             </div>
         );
     }
 }
+
+
 export default App;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,13 +2,17 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        const messages = this.props.messages;
+        const messageList = messages.map((message) =>
+            <Message key={ message.id } username={ message.username } content={ message.content } />
+        );
         return (
             <main className="messages">
-                <Message />
-                <div className="message system">
-                    Anonymous1 changed their name to nomnom.
-                </div>
+                { messageList } 
             </main>
         );
     }
