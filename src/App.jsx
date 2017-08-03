8bb79dc8201dafe1cdff8abf022045facee8e13b
Sam Coll <samcoll@gmail.com>
2017-08-03 13:53:55 -0700

    Setting up the project structure. Adding component .jsx files, filling them with html content, importing them into App.jsx and rendering them into index.html.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <MessageList />
+                <ChatBar />
+            </div>
+        );
+    }
 }
 export default App;
