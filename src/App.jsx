f35491fa48c4973113fce8cccc03272cd468b930
Sam Coll <samcoll@gmail.com>
2017-08-05 13:21:15 -0700

    Adding a new directory chatty_server containing the code for connecting the app to an Express server. Creating a socket object in App.jsx for the purpose of connecting to this server.

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,9 @@ import NavBar from './NavBar.jsx';
 
 class App extends Component {
     componentDidMount() {
-        // console.log("componentDidMount <App />");
-        // setTimeout(() => {
-        //     console.log("Simulating incoming message");
-        //     // Add a new message to the list of messages in the data store
-        //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-        //     const messages = this.state.messages.concat(newMessage);
-        //     // Update the state of the app component.
-        //     // Calling setState will trigger a call to render() in App and all child components.
-        //     this.setState({messages: messages});
-        // }, 3000);
+        this.socket.onopen = function (event) {
+            console.log('Connected to server.'); 
+        };
     }
     constructor(props) {
         super(props);
@@ -34,11 +27,8 @@ class App extends Component {
             ]
         }
         this.sendMessage = this.sendMessage.bind(this);
+        this.socket = new WebSocket("ws://localhost:3001");
     }
-    sendMessage(message) {
-        const messages = this.state.messages.concat(message);
-        this.setState({ messages: messages });
-    } 
     render() {
         return (
             <div>
